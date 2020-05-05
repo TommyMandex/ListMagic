b0ee0cc089ef5f3d1ff6529baa85c258a7581df3
weston2337 <62199874+weston2337@users.noreply.github.com>
2020-05-05 13:50:34 +0300

    Update listmagic.sh
    Finished and fixed last sort of bugs I have found.
    Program should be stable now.

--- listmagic/listmagic.sh
+++ listmagic/listmagic.sh
@@ -33,9 +33,9 @@ if [[ -f $domainlist ]]; then
 	    echo $domainlistLine | cut -d. -f1 | sort -u | tee -a ./results/final/$1_all_firstwords.txt
             echo $domainlistLine | cut -d. -f2 | sort -u | tee -a ./results/final/$1_all_secondwords.txt
             echo $domainlistLine | cut -d. -f3 | sort -u | tee -a ./results/final/$1_all_thirdwords.txt
-	    echo $domainlistLine | cut -d. -f2- | sort -u | tee -a ./results/final/$1_all_first_secondswords.txt
-            echo $domainlistLine | cut -d. -f3- | sort -u | tee -a ./results/final/$1_all_first_second_thirdwords.txt
-	    cat ./results/final/* | sort -u | tee -a ./results/final/$1_all_together.txt
+	    echo $domainlistLine | cut -d. -f-2 | sort -u | tee -a ./results/final/$1_all_first_secondswords.txt
+            echo $domainlistLine | cut -d. -f-3 | sort -u | tee -a ./results/final/$1_all_first_second_thirdwords.txt
+	    cat ./results/final/$1_* | sort -u | tee -a ./results/final/$1_all_together.txt
     done < $domainlist
 
 
@@ -55,7 +55,7 @@ amass enum -d $1 --passive -o ./results/$1_amass.txt
 
 amass enum -df ./results/$1_amass.txt --passive -o ./results/$1_amass_2.txt
 mkdir -p ./results/final/
-cat ./results/*.txt | sort -u | tee -a ./results/final/all.txt
+cat ./results/final/$1_* | sort -u | tee -a ./results/final/all.txt
 
 # The upcoming code can be modified. If you want more words, simply copy one of these lines, replace the 1/2/3 with your desired number where you want the words to be delimited, then look at the results.
 
@@ -65,13 +65,15 @@ cat ./results/final/all.txt | cut -d. -f2 | sort -u | tee -a ./results/final/$1_
 
 cat ./results/final/all.txt | cut -d. -f3 | sort -u | tee -a ./results/final/$1_all_thirdwords.txt
 
-cat ./results/final/all.txt | cut -d. -f2- | sort -u | tee -a ./results/final/$1_all_first_secondswords.txt
+cat ./results/$1_*.txt | cut -d. -f-2 | sort -u | tee -a ./results/final/$1_all_first_secondswords.txt
 
-cat ./results/final/all.txt | cut -d. -f3- | sort -u | tee -a ./results/final/$1_all_first_second_thirdwords.txt
+cat ./results/$1_*.txt | cut -d. -f-3 | sort -u | tee -a ./results/final/$1_all_first_second_thirdwords.txt
 
 cat ./results/final/$1_* | sort -u | tee -a ./results/final/$1_all_together.txt
 
-sed -i "/\b\(.yahoo\|.com\)\b/d" ./results/final/*
+sed -i "/\b\(.\$1\)\b/d" ./results/final/$1_all_together.txt
+extension="$(echo "$1" | rev | cut -d "." -f2 | rev)"
+sed -i "/\b\($extension\)\b/d" ./results/final/$1_all_together.txt
 
 mkdir $1_results
 cat ./results/final/$1_all_together.txt | sort -u | tee -a $1_results/results.txt
